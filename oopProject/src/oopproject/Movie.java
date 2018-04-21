4459eeda571c151b2b0e675317a968a790d62208
Soulid <38246043+Puminan@users.noreply.github.com>
2018-04-21 09:58:02 +0700

    Success

---
+++ oopProject/src/oopproject/Movie.java
@@ -0,0 +1,24 @@
+
+package oopproject;
+
+
+abstract class Movie extends Date {
+    abstract void movie();
+}
+
+abstract class Date {
+    abstract void date();
+}
+
+interface Time {
+    public void time();
+}
+
+interface Seat {
+    public void seat();
+}
+
+interface Cbill {
+    public void cbill();
+}
+
